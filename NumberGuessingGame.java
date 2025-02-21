e96c90f94c965c879f033a5720e8483529e4eada
Marri Sarayu <sarayumarri6625@gmail.com>
2025-02-21 20:24:08 +0530

    NumberGuessingGame.java

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,49 +1,46 @@
-import java.util.Random; 
-import java.util.Scanner; 
+import java.util.Random;
+import java.util.Scanner;
+public class NumberGuessingGame
+ {
 
-public class NumberGuessingGame {
-
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
-        int score = 0; 
-        boolean playAgain = true; 
+        int score = 0;
+        boolean playAgain = true;
 
         while (playAgain) {
-            int secretNumber = r.nextInt(100) + 1; 
+            int secretNumber = r.nextInt(100) + 1;
             int attempts = 0;
-            int maxAttempts = 7; 
+            int maxAttempts = 7;
 
             System.out.println("I've picked a number between 1 and 100. You have " + maxAttempts + " tries.");
 
             while (attempts < maxAttempts) {
-                System.out.print("Your guess: ");  
+                System.out.print("Your guess: ");
                 int guess = sc.nextInt();
                 attempts++;
 
-                if (guess == secretNumber) {  
+                if (guess == secretNumber) {
                     System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
                     score++;
-                    break;  
-                } else if (guess < secretNumber) {  
+                    break;
+                } else if (guess < secretNumber) {
                     System.out.println("Too low!");
-                } else 
-                { 
+                } else {
                     System.out.println("Too high!");
                 }
 
-                if (attempts == maxAttempts) {  
+                if (attempts == maxAttempts) {
                     System.out.println("You ran out of tries. The number was " + secretNumber + ".");
                 }
             }
 
-            System.out.print("Do you want to play again? (Yes/No): ");  
+            System.out.print("Do you want to play again? (Yes/No): ");
             String playChoice = sc.next();
-            playAgain = playChoice.equalsIgnoreCase("Yes");
-
+            playAgain = playChoice.equals("Yes");
         }
-        System.out.println("Thanks for playing! Your final score is: " + score);  
+        System.out.println("Thanks for playing! Your final score is: " + score);
         sc.close();
     }
 }
