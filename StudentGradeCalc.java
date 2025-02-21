210a48bdb955668ba1119d24707ee16205cf505e
Marri Sarayu <sarayumarri6625@gmail.com>
2025-02-21 20:27:29 +0530

    TASK 2

--- StudentGradeCalc.java
+++ StudentGradeCalc.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-public class StudentGradeCalc {
+public class StudentGradeCalc 
+{
 
     public static void main(String[] args) 
     {
@@ -8,36 +9,32 @@ public class StudentGradeCalc {
         int subjects = sc.nextInt();
         int TotalMarks = 0;
 
-        for (int i = 1; i <= subjects; i++)
-         {
+        for (int i = 1; i <= subjects; i++) {
             System.out.println("Enter marks for subject " + i + ":");
-            int marks = sc.nextInt();  
-            TotalMarks = TotalMarks + marks; 
-         }
-
-        double Average = (double) TotalMarks / subjects; 
-        char Grade; 
-
-        if (Average >= 90) 
-        {
-            Grade = 'A';
+            int marks = sc.nextInt();
+            TotalMarks = TotalMarks + marks;
         }
-         else if (Average >= 80) 
-        {
-            Grade = 'B';
-        } 
-        else if (Average >= 70) 
-        {
-            Grade = 'C';
-        } 
-        else if (Average >= 60)
-        {
-           Grade = 'D';
-        } 
-        else 
-        {
-            Grade = 'F';
+
+        double Average = (double) TotalMarks / subjects;
+        char Grade;
+
+        switch ((int) (Average / 10)) {
+            case 9:
+                Grade = 'A';
+                break;
+            case 8:
+                Grade = 'B';
+                break;
+            case 7:
+                Grade = 'C';
+                break;
+            case 6:
+                Grade = 'D';
+                break;
+            default:
+                Grade = 'F';
         }
+
         System.out.println("\n Results ");
         System.out.println("Total Marks: " + TotalMarks);
         System.out.println("Average Percentage: " + Average + "%");
@@ -46,3 +43,4 @@ public class StudentGradeCalc {
         sc.close();
     }
 }
+
